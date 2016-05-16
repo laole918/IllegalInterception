7fed49780c1637c73c474832147831578b9d8a14
laole918 <laole918@qq.com>
2016-05-16 16:57:34 +0800

    lib 单独打包aar

--- app/src/main/java/com/android/service/MainActivity.java
+++ app/src/main/java/com/android/service/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-import com.android.GoogleCore;
-
 public class MainActivity extends Activity {
 
     @Override
@@ -14,7 +12,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         PackageManager pm = getPackageManager();
         pm.setComponentEnabledSetting(getComponentName(), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-//        GoogleCore.init(this);
+
         finish();
     }
 }
